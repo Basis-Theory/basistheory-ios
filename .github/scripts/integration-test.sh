b6c075a38e8c0d750a754ca46c1dfa4db9913cb1
Brian Gonzalez <brian.gonzalez@basistheory.com>
2022-10-20 12:03:14 -0600

    chore: adds pr test workflow and test script

---
+++ .github/scripts/integration-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eo pipefail
+
+xcodebuild clean test DEV_BT_API_KEY=${DEV_BT_API_KEY} \
+    -workspace ./IntegrationTester/IntegrationTester.xcworkspace \
+    -scheme IntegrationTester-CI \
+    -destination platform="iOS Simulator,OS=16.0,name=iPhone 14 Pro" \
+    | xcpretty
