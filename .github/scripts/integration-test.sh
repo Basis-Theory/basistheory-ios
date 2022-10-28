d332500bf1a0cfc0c8eea458af3fa1cd78ecbff8
Brian Gonzalez <brian.gonzalez@basistheory.com>
2022-10-28 16:08:50 -0600

    chore: upgrades simulator to 16.1

--- .github/scripts/integration-test.sh
+++ .github/scripts/integration-test.sh
@@ -6,5 +6,5 @@ xcodebuild clean test DEV_BT_API_KEY=${DEV_BT_API_KEY} \
     -workspace ./IntegrationTester/IntegrationTester.xcworkspace \
     -scheme IntegrationTester-CI \
     -configuration Debug \
-    -destination platform="iOS Simulator,OS=16.0,name=iPhone 14 Pro" \
+    -destination platform="iOS Simulator,OS=16.1,name=iPhone 14 Pro" \
     | xcpretty
