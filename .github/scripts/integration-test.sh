51cbf5f609156be1725541f7c555952886163dd3
Brian Gonzalez <brian.gonzalez@basistheory.com>
2022-10-28 16:21:00 -0600

    chore: adds script to add missing plist

--- .github/scripts/integration-test.sh
+++ .github/scripts/integration-test.sh
@@ -2,6 +2,14 @@
 
 set -eo pipefail
 
+# hack to stop Xcode from complaining about missing plist file
+cat <<EOT > ./IntegrationTester/Env.Local.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+</plist>
+EOT
+
 xcodebuild clean test DEV_BT_API_KEY=${DEV_BT_API_KEY} \
     -workspace ./IntegrationTester/IntegrationTester.xcworkspace \
     -scheme IntegrationTester-CI \
