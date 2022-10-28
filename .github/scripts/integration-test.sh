0cefebf732667006de29d702f48de7bd74423bf0
Brian Gonzalez <brian.gonzalez@basistheory.com>
2022-10-28 15:40:01 -0600

    chore: hoists up scheme to the workspace

--- .github/scripts/integration-test.sh
+++ .github/scripts/integration-test.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 xcodebuild clean test DEV_BT_API_KEY=${DEV_BT_API_KEY} \
     -workspace ./IntegrationTester/IntegrationTester.xcworkspace \
-    -scheme IntegrationTester \
+    -scheme IntegrationTester-CI \
     -destination platform="iOS Simulator,OS=15.2,name=iPhone 13 Pro" \
     | xcpretty
