fa67c8237bc5a80a8a7281be7653f7da93b55830
Brian Gonzalez <brian.gonzalez@basistheory.com>
2022-10-28 15:56:27 -0600

    chore: adds configuration flag

--- .github/scripts/integration-test.sh
+++ .github/scripts/integration-test.sh
@@ -5,5 +5,6 @@ set -eo pipefail
 xcodebuild clean test DEV_BT_API_KEY=${DEV_BT_API_KEY} \
     -workspace ./IntegrationTester/IntegrationTester.xcworkspace \
     -scheme IntegrationTester-CI \
+    -configuration Debug \
     -destination platform="iOS Simulator,OS=15.2,name=iPhone 13 Pro" \
     | xcpretty
