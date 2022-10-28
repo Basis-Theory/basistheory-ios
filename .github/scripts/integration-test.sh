0b9b393d33c2f8a89995f239434b184431754080
Brian Gonzalez <brian.gonzalez@basistheory.com>
2022-10-28 11:37:03 -0600

    chore: uses iPhone 13 Pro for tests

--- .github/scripts/integration-test.sh
+++ .github/scripts/integration-test.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 xcodebuild clean test DEV_BT_API_KEY=${DEV_BT_API_KEY} \
     -workspace ./IntegrationTester/IntegrationTester.xcworkspace \
     -scheme IntegrationTester-CI \
-    -destination platform="iOS Simulator,OS=16.0,name=iPhone 14 Pro" \
+    -destination platform="iOS Simulator,OS=16.0,name=iPhone 13 Pro" \
     | xcpretty
