8a60554b8714d462a386c8a4acf785463051ecb6
Brian Gonzalez <brian.gonzalez@basistheory.com>
2022-10-28 15:28:37 -0600

    chore: trying a default scheme

--- .github/scripts/integration-test.sh
+++ .github/scripts/integration-test.sh
@@ -4,6 +4,5 @@ set -eo pipefail
 
 xcodebuild clean test DEV_BT_API_KEY=${DEV_BT_API_KEY} \
     -workspace ./IntegrationTester/IntegrationTester.xcworkspace \
-    -scheme IntegrationTester-CI \
     -destination platform="iOS Simulator,OS=15.2,name=iPhone 13 Pro" \
     | xcpretty
