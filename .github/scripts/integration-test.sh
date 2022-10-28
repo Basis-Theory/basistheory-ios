fd67c21b4457ceef9b4b57262bf6d2f26100fa5a
Brian Gonzalez <brian.gonzalez@basistheory.com>
2022-10-28 15:30:05 -0600

    chore: trying local scheme

--- .github/scripts/integration-test.sh
+++ .github/scripts/integration-test.sh
@@ -4,5 +4,6 @@ set -eo pipefail
 
 xcodebuild clean test DEV_BT_API_KEY=${DEV_BT_API_KEY} \
     -workspace ./IntegrationTester/IntegrationTester.xcworkspace \
+    -scheme IntegrationTester \
     -destination platform="iOS Simulator,OS=15.2,name=iPhone 13 Pro" \
     | xcpretty
