4f292a30fa5cb86938f2f2517a04d24582521090
Brian Gonzalez <brian.gonzalez@basistheory.com>
2022-10-28 16:04:17 -0600

    chore: updating xcode version

--- .github/scripts/integration-test.sh
+++ .github/scripts/integration-test.sh
@@ -6,5 +6,5 @@ xcodebuild clean test DEV_BT_API_KEY=${DEV_BT_API_KEY} \
     -workspace ./IntegrationTester/IntegrationTester.xcworkspace \
     -scheme IntegrationTester-CI \
     -configuration Debug \
-    -destination platform="iOS Simulator,OS=15.2,name=iPhone 13 Pro" \
+    -destination platform="iOS Simulator,OS=16.0,name=iPhone 14 Pro" \
     | xcpretty
