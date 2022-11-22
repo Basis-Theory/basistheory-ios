d420efeb705f12cfcbc95b0d997acfa0dcb0dd1d
Amber Torres <ambertorres@Ambers-MacBook-Pro.local>
2022-11-22 15:52:58 -0500

    WIP: Add public struct ElementTransform struct

---
+++ BasisTheoryElements/Sources/BasisTheoryElements/ElementTransform.swift
@@ -0,0 +1,13 @@
+//
+//  ElementTransform.swift
+//  
+//
+//  Created by Amber Torres on 11/22/22.
+//
+
+import Foundation
+
+public struct ElementTransform {
+    public var matcher: NSRegularExpression?
+    public var stringReplacement: String? = ""
+}
