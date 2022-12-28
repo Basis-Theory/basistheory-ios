8350a4dcf59759dc7f59a749fb6a438211fda45f
Brian Gonzalez <brian.gonzalez@basistheory.com>
2022-12-28 08:39:24 -0700

    chore: reorganizing tests

--- IntegrationTester/UnitTests/ProxyServiceTests.swift
+++ IntegrationTester/UnitTests/ProxyServiceTests.swift
@@ -1,15 +1,15 @@
 //
-//  ElementServiceTests.swift
-//  IntegrationTesterTests
+//  ProxyServiceTests.swift
+//  UnitTests
 //
-//  Created by Brian Gonzalez on 11/10/22.
+//  Created by Brian Gonzalez on 12/28/22.
 //
 
 import XCTest
 import BasisTheoryElements
 import BasisTheory
 
-final class ElementServiceTests: XCTestCase {
+final class ProxyServiceTests: XCTestCase {
     private final var TIMEOUT_EXPECTATION = 5.0
     
     override func setUpWithError() throws {
@@ -17,72 +17,6 @@ final class ElementServiceTests: XCTestCase {
     }
     
     override func tearDownWithError() throws { }
-    
-    func testTokenizeChecksForApplicationTypeOfPublic() throws {
-        let body: [String: Any] = [
-            "myProp": "myValue"
-        ]
-        
-        let privateApiKey = Configuration.getConfiguration().privateBtApiKey!
-        let tokenizeExpectation = self.expectation(description: "Tokenize")
-        BasisTheoryElements.tokenize(body: body, apiKey: privateApiKey) { data, error in
-            XCTAssertNil(data)
-            XCTAssertEqual(error as! TokenizingError, TokenizingError.applicationTypeNotPublic)
-            
-            tokenizeExpectation.fulfill()
-        }
-        
-        waitForExpectations(timeout: TIMEOUT_EXPECTATION)
-    }
-    
-    func testTokenizeReturnsErrorFromApplicationCheck() throws {
-        let body: [String: Any] = [
-            "myProp": "myValue"
-        ]
-        
-        let tokenizeExpectation = self.expectation(description: "Tokenize")
-        BasisTheoryElements.tokenize(body: body, apiKey: "bad api key") { data, error in
-            XCTAssertNil(data)
-            XCTAssertNotNil(error)
-            
-            tokenizeExpectation.fulfill()
-        }
-        
-        waitForExpectations(timeout: TIMEOUT_EXPECTATION)
-    }
-    
-    func testCreateTokenChecksForApplicationTypeOfPublic() throws {
-        let body = CreateToken(type: "token", data: [
-            "myProp": "myValue"
-        ])
-        
-        let privateApiKey = Configuration.getConfiguration().privateBtApiKey!
-        let tokenizeExpectation = self.expectation(description: "Tokenize")
-        BasisTheoryElements.createToken(body: body, apiKey: privateApiKey) { data, error in
-            XCTAssertNil(data)
-            XCTAssertEqual(error as! TokenizingError, TokenizingError.applicationTypeNotPublic)
-            
-            tokenizeExpectation.fulfill()
-        }
-        
-        waitForExpectations(timeout: TIMEOUT_EXPECTATION)
-    }
-    
-    func testCreateReturnsErrorFromApplicationCheck() throws {
-        let body = CreateToken(type: "token", data: [
-            "myProp": "myValue"
-        ])
-        
-        let tokenizeExpectation = self.expectation(description: "Tokenize")
-        BasisTheoryElements.createToken(body: body, apiKey: "bad api key") { data, error in
-            XCTAssertNil(data)
-            XCTAssertNotNil(error)
-            
-            tokenizeExpectation.fulfill()
-        }
-        
-        waitForExpectations(timeout: TIMEOUT_EXPECTATION)
-    }
     
     func testProxyRequest() throws {
         let privateBtApiKey = Configuration.getConfiguration().privateBtApiKey!
---
+++ IntegrationTester/UnitTests/TokenizeAndCreateTokenServiceTests.swift
@@ -0,0 +1,86 @@
+//
+//  ElementServiceTests.swift
+//  IntegrationTesterTests
+//
+//  Created by Brian Gonzalez on 11/10/22.
+//
+
+import XCTest
+import BasisTheoryElements
+import BasisTheory
+
+final class TokenizeAndCreateTokenServiceTests: XCTestCase {
+    private final var TIMEOUT_EXPECTATION = 5.0
+    
+    override func setUpWithError() throws {
+        BasisTheoryAPI.basePath = "https://api-dev.basistheory.com"
+    }
+    
+    override func tearDownWithError() throws { }
+    
+    func testTokenizeChecksForApplicationTypeOfPublic() throws {
+        let body: [String: Any] = [
+            "myProp": "myValue"
+        ]
+        
+        let privateApiKey = Configuration.getConfiguration().privateBtApiKey!
+        let tokenizeExpectation = self.expectation(description: "Tokenize")
+        BasisTheoryElements.tokenize(body: body, apiKey: privateApiKey) { data, error in
+            XCTAssertNil(data)
+            XCTAssertEqual(error as! TokenizingError, TokenizingError.applicationTypeNotPublic)
+            
+            tokenizeExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: TIMEOUT_EXPECTATION)
+    }
+    
+    func testTokenizeReturnsErrorFromApplicationCheck() throws {
+        let body: [String: Any] = [
+            "myProp": "myValue"
+        ]
+        
+        let tokenizeExpectation = self.expectation(description: "Tokenize")
+        BasisTheoryElements.tokenize(body: body, apiKey: "bad api key") { data, error in
+            XCTAssertNil(data)
+            XCTAssertNotNil(error)
+            
+            tokenizeExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: TIMEOUT_EXPECTATION)
+    }
+    
+    func testCreateTokenChecksForApplicationTypeOfPublic() throws {
+        let body = CreateToken(type: "token", data: [
+            "myProp": "myValue"
+        ])
+        
+        let privateApiKey = Configuration.getConfiguration().privateBtApiKey!
+        let tokenizeExpectation = self.expectation(description: "Tokenize")
+        BasisTheoryElements.createToken(body: body, apiKey: privateApiKey) { data, error in
+            XCTAssertNil(data)
+            XCTAssertEqual(error as! TokenizingError, TokenizingError.applicationTypeNotPublic)
+            
+            tokenizeExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: TIMEOUT_EXPECTATION)
+    }
+    
+    func testCreateReturnsErrorFromApplicationCheck() throws {
+        let body = CreateToken(type: "token", data: [
+            "myProp": "myValue"
+        ])
+        
+        let tokenizeExpectation = self.expectation(description: "Tokenize")
+        BasisTheoryElements.createToken(body: body, apiKey: "bad api key") { data, error in
+            XCTAssertNil(data)
+            XCTAssertNotNil(error)
+            
+            tokenizeExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: TIMEOUT_EXPECTATION)
+    }
+}
