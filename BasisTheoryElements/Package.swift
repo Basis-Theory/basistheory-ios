23d57355f1f8c50a2015ccd98c0e021a4fd3cfef
Brian Gonzalez <brian.gonzalez@basistheory.com>
2023-01-10 14:08:48 -0700

    feat: adds createSession service function

--- BasisTheoryElements/Package.swift
+++ BasisTheoryElements/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/Basis-Theory/basistheory-swift", .exact("0.5.5")),
+         .package(url: "https://github.com/Basis-Theory/basistheory-swift", .exact("0.6.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
---
+++ IntegrationTester/UnitTests/SessionServiceTests.swift
@@ -0,0 +1,36 @@
+//
+//  SessionServiceTests.swift
+//  UnitTests
+//
+//  Created by Brian Gonzalez on 1/10/23.
+//
+
+import XCTest
+import BasisTheoryElements
+import BasisTheory
+
+final class SessionServiceTests: XCTestCase {
+    private final var TIMEOUT_EXPECTATION = 3.0
+    
+    override func setUpWithError() throws {
+        BasisTheoryAPI.basePath = "https://api-dev.basistheory.com"
+    }
+    
+    override func tearDownWithError() throws { }
+    
+    func testCreatingASession() throws {
+        let btApiKey = Configuration.getConfiguration().btApiKey!
+        
+        let createSessionExpectation = self.expectation(description: "Create session")
+        BasisTheoryElements.createSession(apiKey: btApiKey) { data, error in
+            XCTAssertNil(error)
+            XCTAssertNotNil(data?.sessionKey)
+            XCTAssertNotNil(data?.nonce)
+            XCTAssertNotNil(data?.expiresAt)
+            
+            createSessionExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: TIMEOUT_EXPECTATION)
+    }
+}
