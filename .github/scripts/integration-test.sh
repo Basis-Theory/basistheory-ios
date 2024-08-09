672fe7e75c9db326169e043868147f5ce3f25d45
Lucas Chociay <lucas@basistheory.com>
2024-08-09 12:47:40 -0300

    fix: yet another setting

--- .github/scripts/integration-test.sh
+++ .github/scripts/integration-test.sh
@@ -28,8 +28,8 @@ xcodebuild clean test \
     -scheme IntegrationTester \
     -configuration Debug \
     -destination platform="iOS Simulator,OS=16.1,name=iPhone 14 Pro" \
-		CODE_SIGN_STYLE=Manual \
-		CODE_SIGN_IDENTITY="" \
-		CODE_SIGNING_REQUIRED="NO" \
-		CODE_SIGNING_ALLOWED="NO" \
+		CODE_SIGN_STYLE=Automatic \
+		CODE_SIGN_IDENTITY=- \
+		AD_HOC_CODE_SIGNING_ALLOWED=YES \
+		DEVELOPMENT_TEAM=BASISTHEORY \
     | xcpretty
