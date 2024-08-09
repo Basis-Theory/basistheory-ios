8f1fdeab830fd99a653601e7ad7447767fe535ef
Lucas Chociay <lucas@basistheory.com>
2024-08-09 16:16:39 -0300

    chore: select correct ios version

--- .github/scripts/integration-test.sh
+++ .github/scripts/integration-test.sh
@@ -27,5 +27,5 @@ xcodebuild clean test \
     -project ./IntegrationTester/IntegrationTester.xcodeproj \
     -scheme IntegrationTester \
     -configuration Debug \
-    -destination platform="iOS Simulator,OS=16.4,name=iPhone 14 Pro" \
+    -destination platform="iOS Simulator,OS=17.0.1,name=iPhone 14 Pro" \
     | xcpretty
