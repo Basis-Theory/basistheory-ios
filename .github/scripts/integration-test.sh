e175778150afc082240fc3caf789ca228e9bc65a
Lucas Chociay <lucas@basistheory.com>
2024-08-09 11:36:56 -0300

    chore: disable ci code signing

--- .github/scripts/integration-test.sh
+++ .github/scripts/integration-test.sh
@@ -28,4 +28,7 @@ xcodebuild clean test \
     -scheme IntegrationTester \
     -configuration Debug \
     -destination platform="iOS Simulator,OS=16.1,name=iPhone 14 Pro" \
+		CODE_SIGN_IDENTITY="" \
+		CODE_SIGNING_REQUIRED="NO" \
+		CODE_SIGNING_ALLOWED="NO" \
     | xcpretty
