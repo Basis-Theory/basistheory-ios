c1de760ced89e344fbbdcd6888249faf66623e54
Matthew Trisoline <mstrisoline@users.noreply.github.com>
2024-12-10 11:28:18 -0500

    ci: update release notifications (#77)
    * ci: update release notifications
    
    * test: run tests in ios 17
    
    ---------
    
    Co-authored-by: kevin <kevin@basistheory.com>

--- .github/scripts/integration-test.sh
+++ .github/scripts/integration-test.sh
@@ -27,5 +27,5 @@ xcodebuild clean test \
     -project ./IntegrationTester/IntegrationTester.xcodeproj \
     -scheme IntegrationTester \
     -configuration Debug \
-    -destination platform="iOS Simulator,OS=17.0.1,name=iPhone 14 Pro" \
+    -destination platform="iOS Simulator,OS=17.2,name=iPhone 15 Pro" \
     | xcpretty
